3d8b15125a1369bc1dfe8dfd94e3591c642b3ea1
topinkapecy <petanasel@seznam.cz>
2026-06-30 23:02:46 +0200

    almost done

--- BLOGIC_CRM/Controllers/KlientController.cs
+++ BLOGIC_CRM/Controllers/KlientController.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BLOGIC_CRM.Models;
@@ -7,145 +6,152 @@ using BLOGIC_CRM.Data;
 namespace BLOGIC_CRM.Controllers
 {
     public class KlientController : Controller
-{
-    private readonly ApplicationDbContext _context;
-
-    public KlientController(ApplicationDbContext context)
     {
-        _context = context;
-    }
+        private readonly ApplicationDbContext _context;
 
-    // GET: KLIENTS
-    public async Task<IActionResult> Index()    
-    {
-        var data = await _context.Klienti.ToListAsync();
-        return View(data);
-    }
-
-    // GET: KLIENTS/Details/5
-    public async Task<IActionResult> Details(int? id)
-    {
-        if (id == null)
+        public KlientController(ApplicationDbContext context)
         {
-            return NotFound();
+            _context = context;
         }
 
-        var klient = await _context.Klienti
-            .Include(k => k.Smlouvy)
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (klient == null)
+        // GET: KLIENTS
+        public async Task<IActionResult> Index()
         {
-            return NotFound();
+            var data = await _context.Klienti.ToListAsync();
+            return View(data);
         }
 
-        return View(klient);
-    }
-
-    // GET: KLIENTS/Create
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    // POST: KLIENTS/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,Jmeno,Prijmeni,Email,Telefon,RodneCislo,Vek,Smlouvy,CeleJmeno")] Klient klient)
-    {
-        if (ModelState.IsValid)
+        // GET: KLIENTS/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            _context.Add(klient);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        return View(klient);
-    }
-
-    // GET: KLIENTS/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var klient = await _context.Klienti.FindAsync(id);
-        if (klient == null)
-        {
-            return NotFound();
-        }
-        return View(klient);
-    }
-
-    // POST: KLIENTS/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int? id, [Bind("Id,Jmeno,Prijmeni,Email,Telefon,RodneCislo,Vek")] Klient klient)
-    {
-        if (id != klient.Id)
-        {
-            return NotFound();
-        }
-
-        if (ModelState.IsValid)
-        {
-            try
+            if (id == null)
             {
-                _context.Update(klient);
+                return NotFound();
+            }
+
+            var klient = await _context.Klienti
+                .Include(k => k.Smlouvy)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (klient == null)
+            {
+                return NotFound();
+            }
+
+            return View(klient);
+        }
+
+        // GET: KLIENTS/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KLIENTS/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Jmeno,Prijmeni,Email,Telefon,RodneCislo,Vek,Smlouvy,CeleJmeno")] Klient klient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(klient);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(klient);
+        }
+
+        // GET: KLIENTS/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var klient = await _context.Klienti.FindAsync(id);
+            if (klient == null)
+            {
+                return NotFound();
+            }
+            return View(klient);
+        }
+
+        // POST: KLIENTS/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, [Bind("Id,Jmeno,Prijmeni,Email,Telefon,RodneCislo,Vek")] Klient klient)
+        {
+            if (id != klient.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(klient);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KlientExists(klient.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(klient);
+        }
+
+        // GET: KLIENTS/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var klient = await _context.Klienti
+                .Include(k => k.Smlouvy)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (klient == null)
+            {
+                return NotFound();
+            }
+
+            // Klient může mít navázané smlouvy - ty se smažou společně s ním (cascade delete)
+            ViewBag.PocetSmluv = klient.Smlouvy.Count;
+
+            return View(klient);
+        }
+
+        // POST: KLIENTS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var klient = await _context.Klienti
+                .Include(k => k.Smlouvy)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (klient != null)
+            {
+                _context.Klienti.Remove(klient);
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!KlientExists(klient.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+
             return RedirectToAction(nameof(Index));
         }
-        return View(klient);
-    }
 
-    // GET: KLIENTS/Delete/5
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null)
+        private bool KlientExists(int? id)
         {
-            return NotFound();
+            return _context.Klienti.Any(e => e.Id == id);
         }
-
-        var klient = await _context.Klienti
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (klient == null)
-        {
-            return NotFound();
-        }
-
-        return View(klient);
     }
-
-    // POST: KLIENTS/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int? id)
-    {
-        var klient = await _context.Klienti.FindAsync(id);
-        if (klient != null)
-        {
-            _context.Klienti.Remove(klient);
-        }
-
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-    }
-
-    private bool KlientExists(int? id)
-    {
-        return _context.Klienti.Any(e => e.Id == id);
-    }
-}
 }
